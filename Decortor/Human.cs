0f66786ceb3c31ec2ae0d508b7e233ceb9587528
My Name <kiykobogdan@gmail.com>
2023-03-28 23:41:11 +0300

    Add project files.

---
+++ Decortor/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decortor
+{
+    class Human
+    {
+        public Human() :MainCharacter
+        {
+            Name = "Human";
+            Attack = 20;
+            Speed = 20;
+            Health = 150;
+            Armor = 0;
+        }
+    }
+}
